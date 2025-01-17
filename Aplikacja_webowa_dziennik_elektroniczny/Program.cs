b66d0bcc1dfb39fbbedac3f8b6cc00746a0b1b2c
Jakub Tchórzewski <273165@student.pwr.edu.pl>
2025-01-17 20:49:55 +0100

    update app to work with docker

--- Aplikacja_webowa_dziennik_elektroniczny/Program.cs
+++ Aplikacja_webowa_dziennik_elektroniczny/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -32,6 +33,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
